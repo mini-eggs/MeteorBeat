ee2c3142310581049c13e696ed146bedd5e906c4
Nathan Zander <nzander@fenwaygroup.com>
2019-10-03 18:38:00 -0700

    Meteor Beat Version 0

---
+++ MeteorBeat/Assets/Cory/keyControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyControls : MonoBehaviour
+{
+    Rigidbody spaceship;
+    public float flyingSpeed = 40f;
+    public float movementSpeed = 40f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceship = GetComponent<Rigidbody>();
+        spaceship.velocity = Vector3.forward * flyingSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spaceship.velocity = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, Input.GetAxis("Vertical") * movementSpeed, spaceship.velocity.z);
+    }
+}
