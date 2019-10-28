f5d75b8cb43b04ddd64cb68abcebe5c958834635
Clug-Seal <32446406+Clug-Seal@users.noreply.github.com>
2019-10-27 18:24:12 -0700

    Test + Basic Powerup
    The test looks for a powerup to be drawn to screen.
    The Powerup class is the main control of the powerups

---
+++ MeteorBeat/Assets/Caleb/Powerup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using System.Linq;
+
+//Caleb Seely 
+
+public class Powerup : MonoBehaviour
+{
+   // public GameObject pickupEffect;
+    public GameObject Health;
+       
+    void Start()
+    {
+        //print("Hello");
+    }
+
+    private void Update()
+    {
+        Vector3 SpaceshipPosition = GameObject.Find("Spaceship").transform.position;
+        Vector3 PowerupPosition = GameObject.Find("Health").transform.position;
+
+        if (PowerupPosition.z < SpaceshipPosition.z)
+        {
+            Spawn(SpaceshipPosition.z);
+            Debug.Log("Moved powerup");
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision");
+
+        Vector3 SpaceshipPosition = GameObject.Find("Spaceship").transform.position;
+        print("Cords: " + SpaceshipPosition);
+        float Z_Cord = SpaceshipPosition.z;
+        
+        Pickup();
+        Spawn(Z_Cord);
+    }
+
+    void Pickup()
+    {
+        //Will call different commands depending on the powerup type
+    }
+
+    void UsePowerup()
+    {
+        // PlayerStats stats = player.GetComponent<PlayerStats>();
+        //stats.health = health + 20;
+    }
+
+    // This will move the powerup when hit or if the spaceship has passed it.
+    void Spawn(float Z_Cord)
+    {
+        Vector3 Location = new Vector3(-5, 1, Z_Cord + 50);
+        Health.transform.position = new Vector3(-5,1,Z_Cord+50);
+        Debug.Log("Powerup moved");
+    }
+}
---
+++ MeteorBeat/Assets/Caleb/Tests/PowerupSpawnTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEditor; 
+
+
+namespace Tests
+{
+    public class PowerupSpawnTest 
+    {
+        
+        // A Test behaves as an ordinary method
+        [Test]
+        public void PowerupSpawnTestSimplePasses()
+        {
+            //var s = Powerup.Instance;
+            // Use the Assert class to test conditions
+            Debug.Log("Hello World");
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator PowerupSpawnToScreenTest()
+        {
+            Debug.Log("Powerup has been found on screen if this passes");
+            Object PrefabSpawn = Resources.Load("Assets/Health");
+            
+            var PowerupPrefabFind = GameObject.FindWithTag("Health");
+
+            Object PowerupPrefab = PrefabUtility.GetCorrespondingObjectFromOriginalSource(PowerupPrefabFind);
+            
+            Assert.AreEqual(PrefabSpawn, PowerupPrefab);
+
+            //Debug.Log("Name: " + PrefabSpawn.name);
+            return null;
+        }
+    }
+}
