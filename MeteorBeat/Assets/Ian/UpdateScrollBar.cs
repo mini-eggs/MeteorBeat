0eaa00a4a9a590d44d9b947db097f3557417408d
wood3802 <40341288+wood3802@users.noreply.github.com>
2019-10-28 23:09:28 -0700

    Test and Scrollbar
    Added scrollbar for level progression and test for scrollbar to ensure it displays correctly.

---
+++ MeteorBeat/Assets/Ian/UpdateScrollBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UpdateScrollBar : UpdateUIElements
+{
+    float delay = 0.2f;
+    public float startZ, endZ;
+    public GameObject myScrollBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startZ = transform.position.z;
+        StartCoroutine(UpdateUIElement());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override IEnumerator UpdateUIElement()
+    {
+        while (true)
+        {
+            ExecuteEvents.Execute<ICustomUIListener>(myScrollBar, null, (x, y) => x.UpdateUIElement((transform.position.z - startZ) / (endZ - startZ)));
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
