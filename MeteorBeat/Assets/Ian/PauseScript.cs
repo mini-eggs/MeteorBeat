de4df92c6630dea87825093a035c4f2a50ebec07
wood3802 <40341288+wood3802@users.noreply.github.com>
2019-11-10 17:13:53 -0800

    Pause Menu
    Added pause menu so user can reset the level and will eventually be able to go back to the main menu.

---
+++ MeteorBeat/Assets/Ian/PauseScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject pausePlane;
+    public Button restartButton, quitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 1)
+        {
+            pausePlane.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+            quitButton.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 0)
+        {
+            pausePlane.SetActive(false);
+            restartButton.gameObject.SetActive(false);
+            quitButton.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
---
+++ MeteorBeat/Assets/Ian/ResetLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ResetLevel : MonoBehaviour
+{
+    Button myButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myButton = GetComponent<Button>();
+        myButton.onClick.AddListener(ResetLevelFunc);
+    }
+
+    public void ResetLevelFunc()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
