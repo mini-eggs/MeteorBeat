541ddc65f012def39b1949081c08f57fbfc2c2ba
Caleb-Seely <55034908+Caleb-Seely@users.noreply.github.com>
2019-11-14 00:43:49 -0800

    Powerup script and random coordinate generating
    Additional tests were added too

---
+++ PowerupSpawnTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+/* Caleb Seely
+ * Metero Beat Tests
+ * 11/2/19
+ */
+namespace Tests
+{
+    public class PowerupSpawnTest 
+    {
+        [Test]
+        public void PowerupSpawned()
+        {
+            Vector3 PowerupPosition = GameObject.Find("Powerup").transform.position;
+            Assert.IsNotNull(PowerupPosition);
+            //Debug.Log("Powerup did not spawn");
+        }
+
+        /* Boundry Test
+         * Checks if Powerup object was spawned and placed
+         * in a reachable location for the player.
+         */
+        [Test]
+        public void PowerupSpawnAheadofPlayerTest()
+        {
+            Vector3 PowerupPosition = GameObject.Find("Powerup").transform.position;
+            Vector3 SpaceshipPosition = GameObject.Find("Spaceship").transform.position;
+            Assert.IsTrue(PowerupPosition.z > SpaceshipPosition.z);
+            //Debug.Log("Powerup not reachable by the player.");
+        }
+        
+        // Expected to fail
+        [Test]
+        public IEnumerator PowerupSpawnAheadofPlayerFAILTest()
+        {
+            Vector3 PowerupPosition = GameObject.Find("Powerup").transform.position;
+            Vector3 SpaceshipPosition = GameObject.Find("Spaceship").transform.position;
+            Debug.Log("Powerup not reachable by the player. \n This should always fail!");
+            Assert.IsTrue(PowerupPosition.z < SpaceshipPosition.z);
+            return null;
+        }
+    }
+}
