ecb142c85863eadf413115c4fccdc4ecf3131c84
wood3802 <40341288+wood3802@users.noreply.github.com>
2019-11-17 15:12:54 -0800

    Title Screen

---
+++ MeteorBeat/Assets/Ian/TitleScreenMotion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenMotion : MonoBehaviour
+{
+   float speed = 50f;
+   public Vector3 pivot;
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.RotateAround(pivot, -Vector3.forward, speed * Time.deltaTime);
+    }
+}
