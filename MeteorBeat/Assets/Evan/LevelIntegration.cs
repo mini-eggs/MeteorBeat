67772122668923eb5b50329f30c9ed8dcc6e7753
Evan M Jones <me@evanjon.es>
2019-11-17 22:49:27 -0800

    Fix(code styling): Formatting code.

--- MeteorBeat/Assets/Evan/LevelIntegration.cs
+++ MeteorBeat/Assets/Evan/LevelIntegration.cs
@@ -5,55 +5,59 @@ using UnityEngine;
 /*
  * LevelIntegration
  *
- * Meta class. Simply play level soundtrack when level begins and stop playing
- * when level is over. Connect this script to the top level scene within Unity
- * editor.
+ * Meta class. Simply play level soundtrack when level begins and stop 
+ * playing when level is over. Connect this script to the top level 
+ * scene within Unity editor.
  */
 public class LevelIntegration : MonoBehaviour
 {
 
-  private bool isRunning; // for tracking if we need to call UserHasWon
-  
-  /*
-   * Start
-   *
-   * Grab instance of BeatBox singleton and begin playing soundtrack.
-   */
-  void Start()
-  {
-    var s = BeatBox.Instance;
-    s.LoadSounds(this.gameObject); // First sound that plays always. Load all game sounds on start.
-    s.PlayLevelSoundTrack();
-    isRunning = true;
-  }
+   private bool isRunning; // for tracking if we need to call UserHasWon
 
-  /*
-   * Update
-   *
-   * Check if game is over and if so restart it and let ship know.
-   */
-  void Update() 
-  {
-    if (!BeatBox.Instance.IsPlaying() && isRunning)
-    {
-      isRunning = false; // Book keeping, don't enter this conditional twice in a row.
+   /*
+    * Start
+    *
+    * Grab instance of BeatBox singleton and begin playing soundtrack.
+    */
+   void Start()
+   {
+      var s = BeatBox.Instance;
+      // First sound that plays always. Load all game sounds on start.
+      s.LoadSounds(this.gameObject);
+      s.PlayLevelSoundTrack();
+      isRunning = true;
+   }
 
-      // Tell other components game is over.
-      GameObject.FindGameObjectWithTag("Player").GetComponent<ShipIntegration>().UserHasWon();
+   /*
+    * Update
+    *
+    * Check if game is over and if so restart it and let ship know.
+    */
+   void Update()
+   {
+      if (!BeatBox.Instance.IsPlaying() && isRunning)
+      {
+         // Book keeping, don't enter this conditional twice in a row.
+         isRunning = false;
 
-      // Restart the game after three seconds
-      Invoke("RestartGame", 3);
-    }
-  }
+         // Tell other components game is over.
+         GameObject.FindGameObjectWithTag("Player")
+            .GetComponent<ShipIntegration>()
+            .UserHasWon();
 
-  /*
-   * RestartGame
-   *
-   * A simple level restart.
-   */
-  public void RestartGame() 
-  {
-    Application.LoadLevel(Application.loadedLevel);
-  }
+         // Restart the game after three seconds
+         Invoke("RestartGame", 3);
+      }
+   }
+
+   /*
+    * RestartGame
+    *
+    * A simple level restart.
+    */
+   public void RestartGame()
+   {
+      Application.LoadLevel(Application.loadedLevel);
+   }
 
 }
