28426e2fda73e9c6a5b33fb04f568c9aa467f2d5
CozyCatCoder <55250732+CozyCatCoder@users.noreply.github.com>
2019-11-17 18:58:09 -0800

    score and health
    Added health and score variables and implementations

--- MeteorBeat/Assets/Cosette/Score.cs
+++ MeteorBeat/Assets/Cosette/Score.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacles : MonoBehaviour
+public class Score : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- MeteorBeat/Assets/Evan/AsteroidIntegration.cs
+++ MeteorBeat/Assets/Evan/AsteroidIntegration.cs
@@ -9,8 +9,13 @@ using UnityEngine;
  * when level is over. Connect this script to the top level scene within Unity
  * editor.
  */
+
 public class AsteroidIntegration : MonoBehaviour
 {
+  /*cosette's addition: I added a health mechanism
+   * the health says 5 however with the if statement it will be 6 lives
+   */ 
+   static int health = 5;
   /* 
    * OnTriggerEnter
    *
@@ -18,6 +23,12 @@ public class AsteroidIntegration : MonoBehaviour
    * that will handle playing sound/particle effects/other.
    */
   void OnTriggerEnter(){
-    GameObject.FindGameObjectWithTag("Player").GetComponent<ShipIntegration>().collideWithAsteroid();
+    if(health > 0){
+        health --;
+    }
+    else{
+      health = 5;
+         GameObject.FindGameObjectWithTag("Player").GetComponent<ShipIntegration>().collideWithAsteroid();
+    }
   }
 }
