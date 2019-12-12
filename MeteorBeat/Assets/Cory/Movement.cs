ad8a6b9cc7e49167ecfda8e563e2d3ad3b5ddae8
Cory-Summers <summ5671@vandals.uidaho.edu>
2019-12-11 19:27:07 -0800

    Updating Movement

---
+++ MeteorBeat/Assets/Cory/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Object Pattern Class for movement
+//So that auto testing is easier
+public class Movement
+{
+	public float avgNumer = 19;
+	public float avgDemon = 20;
+	public virtual Vector2 Average(Vector2 prev, float x, float y)
+	{
+		return new Vector2();
+	}
+	public virtual Vector3 Calculate(float h, float v, float deltaT)
+	{
+		return new Vector3();
+	}
+}
--- MeteorBeat/Assets/Cory/keyControls.cs
+++ MeteorBeat/Assets/Cory/keyControls.cs
@@ -1,30 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class keyControls : MonoBehaviour
 {
-    Rigidbody spaceship;
-    public float flyingSpeed = 40f;
-    public float movementSpeed = 40f;
-    public Transform shipModel;
-    private float lastAverageX = 0f;
-    private float lastAverageY = 0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        spaceship = GetComponent<Rigidbody>();
-    }
+	Rigidbody spaceship;
+	public float flyingSpeed = 40f;
+	public float movementSpeed = 40f;
+	public  Transform shipModel;
+	public  Movement movement;
+	public Func<Vector2, Vector2, float, float> averager;
+	private Vector2 lastAverage;
+	// Start is called before the first frame update
+	void Start()
+	{
+		spaceship = GetComponent<Rigidbody>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        float averageX = (lastAverageX * 19 + Input.GetAxis("Horizontal")) / 20;
-        float averageY = (lastAverageY * 19 + Input.GetAxis("Vertical")) / 20;
-        
-        spaceship.velocity = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, Input.GetAxis("Vertical") * movementSpeed, flyingSpeed);
-        shipModel.rotation = Quaternion.Euler(-averageY*20,0,-averageX*30);
-        lastAverageX = averageX;
-        lastAverageY = averageY;
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		Vector2 average = movement.Average(lastAverage, Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		float averageX = 0;
+		float averageY = 0; ;// = (lastAverageY * 19 + Input.GetAxis("Vertical")) / 20;
+
+		spaceship.velocity = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, Input.GetAxis("Vertical") * movementSpeed, flyingSpeed);
+		shipModel.rotation = Quaternion.Euler(-averageY * 20, 0, -averageX * 30);
+		lastAverage = average;
+	}
 }
